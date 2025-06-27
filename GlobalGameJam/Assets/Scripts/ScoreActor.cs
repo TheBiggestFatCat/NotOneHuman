ac7f08ff46813bfde9c7e6fe978dd8f5d998436f
张齐煜 <9090085+little_redtea@user.noreply.gitee.com>
2025-06-27 21:52:25 +0800

    计分板

--- GlobalGameJam/Assets/Scripts/ScoreActor.cs
+++ GlobalGameJam/Assets/Scripts/ScoreActor.cs
@@ -2,15 +2,58 @@ using UnityEngine;
 
 public class ScoreActor : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static ScoreActor Instance { get; private set; }
+    private int p1Score = 0;
+    private int p2Score = 0;
+
+    void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddP1Score(int value)
     {
-        
+        p1Score += value;
+    }
+
+    public void AddP2Score(int value)
+    {
+        p2Score += value;
+    }
+
+    public void SubtractP1Score(int value)
+    {
+        p1Score -= value;
+        if (p1Score < 0) p1Score = 0;
+    }
+
+    public void SubtractP2Score(int value)
+    {
+        p2Score -= value;
+        if (p2Score < 0) p2Score = 0;
+    }
+
+    public int GetP1Score()
+    {
+        return p1Score;
+    }
+
+    public int GetP2Score()
+    {
+        return p2Score;
+    }
+
+    public void ResetScores()
+    {
+        p1Score = 0;
+        p2Score = 0;
     }
 }
