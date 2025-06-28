7e1e002b2f3c3134d4d0e915d9e5b27c7b6d092f
ZhangZH <443581374@qq.com>
2025-06-28 19:39:06 +0800

    攻击判定框

--- GlobalGameJam/Assets/Scripts/Attacker.cs
+++ GlobalGameJam/Assets/Scripts/Attacker.cs
@@ -2,5 +2,18 @@ using UnityEngine;
 
 public class Attacker : Actor
 {
-    
+    private void OnDrawGizmos()
+    {
+        Vector2 dir = transform.parent.right * -1;
+        Vector2 point = (dir * atkDistance) + new Vector2(transform.position.x, transform.position.y) + atkPoint;
+        Gizmos.color = Color.red;
+
+        Matrix4x4 oldMatrix = Gizmos.matrix;
+        Gizmos.matrix = Matrix4x4.TRS(
+        point,
+        Quaternion.Euler(0, 0, atkAngle),
+        Vector3.one
+        );
+        Gizmos.DrawWireCube(Vector3.zero, atkSize);
+    }
 }
--- GlobalGameJam/Assets/Scripts/Defender.cs
+++ GlobalGameJam/Assets/Scripts/Defender.cs
@@ -13,4 +13,19 @@ public class Defender : Actor
         base.Attack();
         CanAttack = false;
     }
+
+    private void OnDrawGizmos()
+    {
+        Vector2 dir = transform.parent.right * -1;
+        Vector2 point = (dir * atkDistance) + new Vector2(transform.position.x, transform.position.y) + atkPoint;
+        Gizmos.color = Color.red;
+
+        Matrix4x4 oldMatrix = Gizmos.matrix;
+        Gizmos.matrix = Matrix4x4.TRS(
+        point,
+        Quaternion.Euler(0, 0, atkAngle),
+        Vector3.one
+        );
+        Gizmos.DrawWireCube(Vector3.zero, atkSize);
+    }
 }
