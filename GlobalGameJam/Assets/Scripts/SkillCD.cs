820e5d6bed75e427b445d877f2bb8369c88e5a4e
张齐煜 <9090085+little_redtea@user.noreply.gitee.com>
2025-06-28 17:37:31 +0800

    UI

---
+++ GlobalGameJam/Assets/Scripts/SkillCD.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillCD : MonoBehaviour
+{
+    public int PlayerIndex;
+    public Image SkillImage;
+    public Actor PlayerActor;
+    void Update()
+    {
+        Actor actor = GameManager.Instance.GetActor(PlayerIndex);
+        if (actor != null)
+        {
+            actor.UpdateAttackCD.AddListener(SetFillAmount);
+        }
+        else
+        {
+            return;
+        }
+    }
+    public void SetFillAmount(float fillAmount)
+    {
+        if (SkillImage != null)
+        {
+            SkillImage.fillAmount = fillAmount;
+        }
+    }
+}
--- GlobalGameJam/Assets/Scripts/UIController.cs
+++ GlobalGameJam/Assets/Scripts/UIController.cs
@@ -4,14 +4,59 @@ using DG.Tweening;
 public class UIController : MonoBehaviour
 {
     public GameObject gameNameText;
+    public GameObject MenuUI;
+    public GameObject InGameUI;
+    public GameObject Player1UI;
+    public GameObject Player2UI;
 
     void Start()
     {
-        ShowStartMenu();
+        ShowStartText();
+        GameManager.Instance.OnGameReady.AddListener(HideMenuUI);
     }
 
-    public void ShowStartMenu()
+    void Update()
+    {
+        if (GameManager.Instance.GetActor(0) != null)
+        {
+            ShowPlayer1UI();
+        }
+        else
+        {
+            return;
+        }
+        if (GameManager.Instance.GetActor(1) != null)
+        {
+            ShowPlayer2UI();
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    public void ShowStartText()
     {
         gameNameText.transform.DOScale(Vector3.one, 2f);
+    }
+
+    public void HideMenuUI()
+    {
+        MenuUI.transform.DOScale(Vector3.zero, 0.5f);
+        Debug.Log("OnGameReady");
+    }
+
+    public void ShowInGameUI()
+    {
+        InGameUI.transform.DOScale(new Vector3(1, 1, 1), 0.5f);
+    }
+
+    public void ShowPlayer1UI()
+    {
+        Player1UI.transform.DOScale(Vector3.one, 0.5f);
+    }
+    public void ShowPlayer2UI()
+    {
+        Player2UI.transform.DOScale(Vector3.one, 0.5f);
     }
 }
