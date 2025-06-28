e7822bfbcbb8a9cd5c824e1b75d7f80cda896885
张齐煜 <9090085+little_redtea@user.noreply.gitee.com>
2025-06-28 12:21:00 +0800

    1

---
+++ GlobalGameJam/Assets/Scripts/ActorAnimationController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ActorAnimationController: MonoBehaviour
+{
+    public Animator animator;
+    public int blinkChance = 10; // 眨眼的概率
+    public int lookChance = 10; // 左右看的概率
+
+    void Start()
+    {
+        StartCoroutine(Idle());
+    }
+
+    // 待机动画 随机事件眨眼和左右看
+    public IEnumerator Idle()
+    {
+        while (true)
+        {
+            int blinkRand = Random.Range(0, 100);
+            // 随机眨眼
+            if (blinkRand < blinkChance) // 10% 概率眨眼
+            {
+                animator.SetTrigger("blink");
+            }
+
+            int lookRand = Random.Range(0, 100);
+
+            // 随机左右看
+            if (lookRand < lookChance) // 10% 概率左右看
+            {
+                animator.SetTrigger("evil");
+            }
+
+            yield return new WaitForSeconds(1f); // 每隔1秒检查一次
+        }
+    }
+
+    public void OnMove(InputValue value)
+    {
+        Debug.Log(value);
+        animator.SetBool("isMoving", true);
+    }
+    
+
+}
--- GlobalGameJam/Assets/Scripts/Defender.cs
+++ GlobalGameJam/Assets/Scripts/Defender.cs
@@ -2,15 +2,10 @@ using UnityEngine;
 
 public class Defender : Actor
 {
+    public Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Debug.Log("Defender started");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
